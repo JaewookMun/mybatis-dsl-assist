99b571b5d589108801719a996f631619dc9fe174
JaewookMun <mun1103.dev@gmail.com>
2025-01-24 15:11:53 +0900

    feat: 추가적인 기능을 위한 어노테이션 추가 (& 어노테이션 위치 변경)
    - '필드(컬럼)의 식별키 여부'와 '생성값 반환(generatedKeys)' 기능을 추가하기 위한 어노테이션 추가
    - 어노테이션 위치 변경 (1. target 2. retention)

--- src/main/java/github/jaewookmun/mybatis/dsl/assist/Id.java
+++ src/main/java/github/jaewookmun/mybatis/dsl/assist/Id.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.SOURCE)
-public @interface DynamicModel {
-    String table() default "";
+public @interface Id {
+    boolean isGeneratedKey() default false;
 }
